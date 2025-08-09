80ab84534e7d7e3a89c9f6599b60eddde21f4684
mshdabiola <mshdabiola@gmail.com>
2025-08-09 11:04:39 +0100

    Refactor: Update Maven Local configuration and documentation
    This commit updates the project's configuration regarding Maven Local.
    
    - Modified `settings.gradle.kts` to comment out `mavenLocal()` from the plugin management repositories.
    - Updated `instuctions` file to clarify the `publishToMavenLocal` command description to "Publishes artifacts to the local Maven repository."

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ pluginManagement {
         // maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         google()
         mavenCentral()
-        mavenLocal()
+//        mavenLocal()
         gradlePluginPortal()
     }
 }
