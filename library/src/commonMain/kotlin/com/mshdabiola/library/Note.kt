945ead6c5d560166ba62bf0d865fb1a0ad588007
mshdabiola <mshdabiola@gmail.com>
2025-08-21 14:52:29 +0100

    Refactor: Relocate Note model and test tags to library module
    This commit moves the `Note` data class and `NoteCardTestTags` object from the `com.mshdabiola.model` and `com.mshdabiola.model.testtag` packages respectively, to the `com.mshdabiola.library` package.
    
    The import statements in `NoteCard.kt` and `NoteCardTest.kt` have been updated to reflect this change.
    
    - Moved `Note.kt` from `com.mshdabiola.model` to `com.mshdabiola.library`.
    - Moved `NoteCardTestTags.kt` from `com.mshdabiola.model.testtag` to `com.mshdabiola.library`.
    - Updated imports in `library/src/commonMain/kotlin/com/mshdabiola/library/NoteCard.kt`.
    - Updated imports in `library/src/jvmTest/kotlin/com/mshdabiola/library/NoteCardTest.kt`.

--- library/src/commonMain/kotlin/com/mshdabiola/library/Note.kt
+++ library/src/commonMain/kotlin/com/mshdabiola/library/Note.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.mshdabiola.model
+package com.mshdabiola.library
 
 data class Note(
     val id: Long = -1,
